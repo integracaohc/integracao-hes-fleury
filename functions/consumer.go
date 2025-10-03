088c2e45f5b3c90ebeda82a9a4b8bd50e98a1dde
maiconfr <38378643+maiconfr@users.noreply.github.com>
2025-10-03 16:19:25 -0300

    alterado formato para enviar mais instrucoes na procedure, ou seja, para um produto a procedure e ativada n vezes de instrucoes que ela contém

--- functions/consumer.go
+++ functions/consumer.go
@@ -24,8 +24,9 @@ import (
 // }
 
 type INSTRUCAO_FLEURY_MV struct {
-	IdProduto      string `json:"idProduto"`
-	TextoInstrucao string `json:"textoInstrucao"`
+	IdProduto        string `json:"idProduto"`
+	TextoInstrucao   string `json:"textoInstrucao"`
+	IdInstrucaoGeral string `json:"idInstrucaoGeral"`
 }
 
 type GrantCode struct {
@@ -117,7 +118,7 @@ func Consumer() {
 		defer ticker.Stop()
 
 		listaProdutosErros := []ERROS_INTEGRA{}
-		listaProdutosSemInstrucao := []ERROS_INTEGRA{}
+		//listaProdutosSemInstrucao := []ERROS_INTEGRA{}
 
 		for {
 			select {
@@ -128,7 +129,19 @@ func Consumer() {
 					continue
 				}
 
-				instrucaoFleury, err := acessoFleury(cdDepara)
+				// instrucaoFleury, err := acessoFleury(cdDepara)
+				// if err != nil {
+				// 	utils.LogMonitor(utils.ErroFleury, label, "Erro ao acessar Fleury: "+err.Error())
+				// 	if cdDepara.REPETICOES_FILA > 3 {
+				// 		listaProdutosErros = append(listaProdutosErros, ERROS_INTEGRA{
+				// 			CD_DEPARA_INTEGRA: cdDepara.CD_DEPARA_INTEGRA,
+				// 			ERROS:             "Erro ao acessar Fleury: " + err.Error(),
+				// 		})
+				// 	}
+				// 	continue
+				// }
+
+				accessToken, err := acessoFleury()
 				if err != nil {
 					utils.LogMonitor(utils.ErroFleury, label, "Erro ao acessar Fleury: "+err.Error())
 					if cdDepara.REPETICOES_FILA > 3 {
@@ -139,33 +152,45 @@ func Consumer() {
 					}
 					continue
 				}
+				erroProcedure := buscaProcessaInstrucao(cdDepara.CD_DEPARA_INTEGRA, accessToken)
 
-				if instrucaoFleury.IdProduto == "" {
-					utils.LogMonitor(utils.ErroFleury, label, "Produto "+cdDepara.CD_DEPARA_INTEGRA+" nao encontrado no Fleury")
-					listaProdutosSemInstrucao = append(listaProdutosSemInstrucao, ERROS_INTEGRA{
-						CD_DEPARA_INTEGRA: cdDepara.CD_DEPARA_INTEGRA,
-						ERROS:             "Produto nao encontrado no Fleury",
-					})
+				if erroProcedure != "" {
+					utils.LogMonitor(utils.ErroFleury, label, "Erro ao processar instrucao: "+erroProcedure)
+					if cdDepara.REPETICOES_FILA > 3 {
+						listaProdutosErros = append(listaProdutosErros, ERROS_INTEGRA{
+							CD_DEPARA_INTEGRA: cdDepara.CD_DEPARA_INTEGRA,
+							ERROS:             "Erro ao processar instrucao: " + erroProcedure,
+						})
+					}
 					continue
 				}
 
-				if instrucaoFleury.TextoInstrucao == "" {
-					utils.LogMonitor(utils.ErroFleury, label, "Produto "+cdDepara.CD_DEPARA_INTEGRA+" sem instrução no Fleury")
-					listaProdutosSemInstrucao = append(listaProdutosSemInstrucao, ERROS_INTEGRA{
-						CD_DEPARA_INTEGRA: cdDepara.CD_DEPARA_INTEGRA,
-						ERROS:             "Produto sem instrução no Fleury",
-					})
-					continue
-				}
+				// if instrucaoFleury.IdProduto == "" {
+				// 	utils.LogMonitor(utils.ErroFleury, label, "Produto "+cdDepara.CD_DEPARA_INTEGRA+" nao encontrado no Fleury")
+				// 	listaProdutosSemInstrucao = append(listaProdutosSemInstrucao, ERROS_INTEGRA{
+				// 		CD_DEPARA_INTEGRA: cdDepara.CD_DEPARA_INTEGRA,
+				// 		ERROS:             "Produto nao encontrado no Fleury",
+				// 	})
+				// 	continue
+				// }
 
-				if err := procedureMV(instrucaoFleury); err != nil {
-					utils.LogMonitor(utils.ErroIntegracao, label, "Erro ao executar procedure para produto: "+cdDepara.CD_DEPARA_INTEGRA+" - "+err.Error())
-					listaProdutosErros = append(listaProdutosErros, ERROS_INTEGRA{
-						CD_DEPARA_INTEGRA: cdDepara.CD_DEPARA_INTEGRA,
-						ERROS:             "Erro ao executar procedure: " + err.Error(),
-					})
-					continue
-				}
+				// if instrucaoFleury.TextoInstrucao == "" {
+				// 	utils.LogMonitor(utils.ErroFleury, label, "Produto "+cdDepara.CD_DEPARA_INTEGRA+" sem instrução no Fleury")
+				// 	listaProdutosSemInstrucao = append(listaProdutosSemInstrucao, ERROS_INTEGRA{
+				// 		CD_DEPARA_INTEGRA: cdDepara.CD_DEPARA_INTEGRA,
+				// 		ERROS:             "Produto sem instrução no Fleury",
+				// 	})
+				// 	continue
+				// }
+
+				// if err := procedureMV(instrucaoFleury); err != nil {
+				// 	utils.LogMonitor(utils.ErroIntegracao, label, "Erro ao executar procedure para produto: "+cdDepara.CD_DEPARA_INTEGRA+" - "+err.Error())
+				// 	listaProdutosErros = append(listaProdutosErros, ERROS_INTEGRA{
+				// 		CD_DEPARA_INTEGRA: cdDepara.CD_DEPARA_INTEGRA,
+				// 		ERROS:             "Erro ao executar procedure: " + err.Error(),
+				// 	})
+				// 	continue
+				// }
 
 			case <-ticker.C:
 				mensagem := ""
@@ -175,16 +200,16 @@ func Consumer() {
 						mensagem += "\n" + produto.CD_DEPARA_INTEGRA + " - " + produto.ERROS
 					}
 				}
-				if len(listaProdutosSemInstrucao) > 0 {
-					mensagem += "\n\nProdutos sem instrucao: " + strconv.Itoa(len(listaProdutosSemInstrucao))
-					for _, produto := range listaProdutosSemInstrucao {
-						mensagem += "\n" + produto.CD_DEPARA_INTEGRA
-					}
-				}
+				// if len(listaProdutosSemInstrucao) > 0 {
+				// 	mensagem += "\n\nProdutos sem instrucao: " + strconv.Itoa(len(listaProdutosSemInstrucao))
+				// 	for _, produto := range listaProdutosSemInstrucao {
+				// 		mensagem += "\n" + produto.CD_DEPARA_INTEGRA
+				// 	}
+				// }
 				if len(mensagem) > 0 {
 					mail.Send("Subject: Produtos com erros Fleury \n\n Abaixo os produtos com erros: " + mensagem)
 					listaProdutosErros = nil
-					listaProdutosSemInstrucao = nil
+					//listaProdutosSemInstrucao = nil
 				}
 			}
 		}
@@ -220,9 +245,9 @@ func reenviaParaFila(mensagem DEPARA_INTEGRA, ch *amqp091.Channel, q amqp091.Que
 	}
 }
 
-func acessoFleury(depara DEPARA_INTEGRA) (INSTRUCAO_FLEURY_MV, error) {
+func acessoFleury() (string, error) {
 	label := "AcessoFleury"
-	produto := depara.CD_DEPARA_INTEGRA
+	//produto := depara.CD_DEPARA_INTEGRA
 	url := os.Getenv("FLEURY_URL") + `/oauth/grant-code`
 	method := "POST"
 
@@ -242,7 +267,7 @@ func acessoFleury(depara DEPARA_INTEGRA) (INSTRUCAO_FLEURY_MV, error) {
 	if err != nil {
 		fmt.Println(err)
 		utils.LogMonitor(utils.ErroFleury, label, "Erro ao criar request: "+err.Error())
-		return INSTRUCAO_FLEURY_MV{}, err
+		return "", err
 	}
 	req.Header.Add("Content-Type", "application/json")
 
@@ -250,7 +275,7 @@ func acessoFleury(depara DEPARA_INTEGRA) (INSTRUCAO_FLEURY_MV, error) {
 	if err != nil {
 		fmt.Println(err)
 		utils.LogMonitor(utils.ErroFleury, label, "Erro ao fazer request: "+err.Error())
-		return INSTRUCAO_FLEURY_MV{}, err
+		return "", err
 	}
 	defer res.Body.Close()
 
@@ -272,7 +297,7 @@ func acessoFleury(depara DEPARA_INTEGRA) (INSTRUCAO_FLEURY_MV, error) {
 	if err != nil {
 		fmt.Println(err)
 		utils.LogMonitor(utils.ErroFleury, label, "Erro ao criar request: "+err.Error())
-		return INSTRUCAO_FLEURY_MV{}, err
+		return "", err
 	}
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Add("client_id", os.Getenv("FLEURY_CLIENT_ID_GRANT_CODE"))
@@ -282,7 +307,7 @@ func acessoFleury(depara DEPARA_INTEGRA) (INSTRUCAO_FLEURY_MV, error) {
 	if err != nil {
 		fmt.Println(err)
 		utils.LogMonitor(utils.ErroFleury, label, "Erro ao fazer request: "+err.Error())
-		return INSTRUCAO_FLEURY_MV{}, err
+		return "", err
 	}
 	defer res.Body.Close()
 
@@ -291,38 +316,44 @@ func acessoFleury(depara DEPARA_INTEGRA) (INSTRUCAO_FLEURY_MV, error) {
 	if err != nil {
 		fmt.Println(err)
 		utils.LogMonitor(utils.ErroFleury, label, "Erro ao ler resposta: "+err.Error())
-		return INSTRUCAO_FLEURY_MV{}, err
+		return "", err
 	}
 	fmt.Println(string(body))
 	json.Unmarshal(body, &accessToken)
 
-	url = os.Getenv("FLEURY_URL") + `/instrucoes-gerais-hospitais/v1/produtos?produtos=` + produto
-	method = "GET"
+	return accessToken.AccessToken, nil
 
-	client = &http.Client{}
-	req, err = http.NewRequest(method, url, nil)
+}
+
+func buscaProcessaInstrucao(produto string, accessToken string) string {
+	label := "BuscaProcessaInstrucao"
+	url := os.Getenv("FLEURY_URL") + `/instrucoes-gerais-hospitais/v1/produtos?produtos=` + produto
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
 
 	if err != nil {
 		fmt.Println(err)
 		utils.LogMonitor(utils.ErroFleury, label, "Erro ao criar request: "+err.Error())
-		return INSTRUCAO_FLEURY_MV{}, err
+		return "Erro ao criar request: " + err.Error()
 	}
 	req.Header.Add("client_id", os.Getenv("FLEURY_CLIENT_ID_GRANT_CODE"))
-	req.Header.Add("access_token", accessToken.AccessToken)
+	req.Header.Add("access_token", accessToken)
 
-	res, err = client.Do(req)
+	res, err := client.Do(req)
 	if err != nil {
 		fmt.Println(err)
 		utils.LogMonitor(utils.ErroFleury, label, "Erro ao fazer request: "+err.Error())
-		return INSTRUCAO_FLEURY_MV{}, err
+		return "Erro ao fazer request: " + err.Error()
 	}
 	defer res.Body.Close()
 
-	body, err = io.ReadAll(res.Body)
+	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		fmt.Println(err)
 		utils.LogMonitor(utils.ErroFleury, label, "Erro ao ler resposta: "+err.Error())
-		return INSTRUCAO_FLEURY_MV{}, err
+		return "Erro ao ler resposta: " + err.Error()
 	}
 	//fmt.Println(string(body))
 	//log.Println(string(body))
@@ -332,28 +363,42 @@ func acessoFleury(depara DEPARA_INTEGRA) (INSTRUCAO_FLEURY_MV, error) {
 	// log.Println(mensagem)
 	var result []map[string]interface{}
 	if err := json.Unmarshal(body, &result); err != nil {
-		return INSTRUCAO_FLEURY_MV{}, err
+		return "Erro ao deserializar resposta: " + err.Error()
 	}
 	if len(result) == 0 {
-		return INSTRUCAO_FLEURY_MV{}, nil
+		return "Produto nao encontrado no Fleury"
 	}
 	//fmt.Println("idProduto:", result[0]["idProduto"])
 	instrucoes := result[0]["instrucoesGerais"].([]interface{})
 	var instrucoesMV INSTRUCAO_FLEURY_MV
 	instrucoesMV.IdProduto = produto
+
+	erroProcedure := ""
+
 	for _, i := range instrucoes {
 
 		instrucao := i.(map[string]interface{})
 		//fmt.Println(instrucao["idInstrucaoGeral"])
-		if instrucao["idInstrucaoGeral"] != float64(6) {
-			continue
-		}
+		// if instrucao["idInstrucaoGeral"] != float64(6) {
+		// 	continue
+		// }
 		instrucoesMV.TextoInstrucao = instrucao["textoInstrucao"].(string)
+		instrucoesMV.IdInstrucaoGeral = strconv.Itoa(int(instrucao["idInstrucaoGeral"].(float64)))
 		//fmt.Println("idInstrucaoGeral:", instrucao["idInstrucaoGeral"])
 		//fmt.Println("textoInstrucao:", instrucao["textoInstrucao"])
+
+		utils.LogMonitor(utils.Debug, label, "IdProduto: "+produto)
+		utils.LogMonitor(utils.Debug, label, "TextoInstrucao: "+instrucoesMV.TextoInstrucao)
+		utils.LogMonitor(utils.Debug, label, "IdInstrucaoGeral: "+instrucoesMV.IdInstrucaoGeral)
+
+		err = procedureMV(instrucoesMV)
+		if err != nil {
+			utils.LogMonitor(utils.ErroIntegracao, label, "Erro ao executar procedure: "+err.Error())
+			erroProcedure += "Erro ao executar procedure para idInstrucao: " + instrucoesMV.IdInstrucaoGeral + " - " + err.Error() + "\n"
+		}
 	}
 	//log.Println(instrucoesMV)
-	return instrucoesMV, nil
+	return erroProcedure
 }
 
 func procedureMV(instrucao INSTRUCAO_FLEURY_MV) error {
@@ -366,7 +411,7 @@ func procedureMV(instrucao INSTRUCAO_FLEURY_MV) error {
 	}
 	defer db.Close()
 
-	stmt, err := db.Prepare("begin mvintegra.HC_IH_999(:1, :2); end;")
+	stmt, err := db.Prepare("begin mvintegra.HC_IH_999(:1, :2, :3); end;")
 	if err != nil {
 		//log.Println(err.Error() + " - Erro ao preparar a procedure")
 		utils.LogMonitor(utils.ErroBanco, label, "Erro ao preparar a procedure: "+err.Error())
@@ -374,7 +419,7 @@ func procedureMV(instrucao INSTRUCAO_FLEURY_MV) error {
 	}
 	defer stmt.Close()
 
-	_, err = stmt.Exec(instrucao.IdProduto, instrucao.TextoInstrucao)
+	_, err = stmt.Exec(instrucao.IdProduto, instrucao.TextoInstrucao, instrucao.IdInstrucaoGeral)
 	if err != nil {
 		//log.Println(err.Error() + " - Erro ao executar a procedure")
 		utils.LogMonitor(utils.ErroBanco, label, "Erro ao executar a procedure: "+err.Error())
--- functions/enviosManuais.go
+++ functions/enviosManuais.go
@@ -1,12 +1,18 @@
 package functions
 
 import (
+	"context"
+	"integracao-hes-fleury/db"
 	"integracao-hes-fleury/utils"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/godror/godror"
 )
+
+type CD_DEPARA struct {
+	CD_DEPARA_INTEGRA string `json:"cd_depara_integra"`
+}
 
 func ProcessaProdutosFleury() gin.HandlerFunc {
 	fn := func(c *gin.Context) {
@@ -26,38 +32,73 @@ func ProdutosFleury() gin.HandlerFunc {
 		//log.Println("Produto:", produto)
 		utils.LogMonitor(utils.Debug, label, "Produto: "+produto)
 
+		db, err := db.InitDB()
+		if err != nil {
+			//log.Println(err.Error() + " - Erro ao conectar no Oracle")
+			utils.LogMonitor(utils.ErroBanco, label, "Erro ao conectar no Oracle: "+err.Error())
+			return
+		}
+		defer db.Close()
+		ctx := context.Background()
+
+		var produtoFleury CD_DEPARA
+		// row, err := db.Query("select CD_DEPARA_INTEGRA from mvintegra.depara where cd_depara_mv = :1", produto)
+		// if err != nil {
+		// 	//log.Println(err.Error() + " - Erro ao buscar registros no banco")
+		// 	utils.LogMonitor(utils.ErroBanco, label, "Erro ao buscar registros no banco: "+err.Error())
+		// 	return
+		// }
+		// defer row.Close()
+
+		// row.Scan(&produtoFleury.CD_DEPARA_INTEGRA)
+
+		err = db.QueryRowContext(ctx, `select CD_DEPARA_INTEGRA from mvintegra.depara where cd_depara_mv = :1`, produto).Scan(&produtoFleury.CD_DEPARA_INTEGRA)
+		if err != nil {
+			//log.Println(err.Error() + " - Erro ao buscar registros no banco")
+			utils.LogMonitor(utils.ErroBanco, label, "Erro ao buscar registros no banco: "+err.Error())
+			return
+		}
+
 		depara := DEPARA_INTEGRA{
-			CD_DEPARA_INTEGRA: produto,
+			CD_DEPARA_INTEGRA: produtoFleury.CD_DEPARA_INTEGRA,
 			REPETICOES_FILA:   0,
 		}
+		utils.LogMonitor(utils.Debug, label, "Produto encontrado no banco: "+depara.CD_DEPARA_INTEGRA)
+		// accessToken, err := acessoFleury()
+		// if err != nil {
+		// 	//log.Println("Erro ao acessar Fleury:", err)
+		// 	utils.LogMonitor(utils.ErroFleury, label, "Erro ao acessar Fleury: "+err.Error())
+		// 	c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+		// 		"sucesso": false,
+		// 		"message": "Erro ao acessar Fleury " + err.Error(),
+		// 	})
+		// 	return
 
-		instrucaoFleury, err := acessoFleury(depara)
+		// }
+		// //utils.LogMonitor(utils.Debug, label, "Produto encontrado no Fleury: "+instrucaoFleury.IdProduto)
+		// if instrucaoFleury.IdProduto == "" {
+		// 	c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+		// 		"sucesso": false,
+		// 		"message": "Produto não encontrado no Fleury",
+		// 	})
+		// 	return
+		// }
+
+		accessToken, err := acessoFleury()
 		if err != nil {
-			//log.Println("Erro ao acessar Fleury:", err)
 			utils.LogMonitor(utils.ErroFleury, label, "Erro ao acessar Fleury: "+err.Error())
-			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
-				"sucesso": false,
-				"message": "Erro ao acessar Fleury " + err.Error(),
-			})
-			return
-
+			// if cdDepara.REPETICOES_FILA > 3 {
+			// 	listaProdutosErros = append(listaProdutosErros, ERROS_INTEGRA{
+			// 		CD_DEPARA_INTEGRA: cdDepara.CD_DEPARA_INTEGRA,
+			// 		ERROS:             "Erro ao acessar Fleury: " + err.Error(),
+			// 	})
+			// }
+			//continue
 		}
-		if instrucaoFleury.IdProduto == "" {
-			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
-				"sucesso": false,
-				"message": "Produto não encontrado no Fleury",
-			})
-			return
-		}
-		//log.Println(mensagem)
-		//log.Println("IdProduto:", instrucaoFleury.IdProduto)
-		//log.Println("TextoInstrucao:", instrucaoFleury.TextoInstrucao)
-		utils.LogMonitor(utils.Debug, label, "IdProduto: "+instrucaoFleury.IdProduto)
-		utils.LogMonitor(utils.Debug, label, "TextoInstrucao: "+instrucaoFleury.TextoInstrucao)
+		erroProcedure := buscaProcessaInstrucao(depara.CD_DEPARA_INTEGRA, accessToken)
 
-		err = procedureMV(instrucaoFleury)
-		if err != nil {
-			//log.Println("Erro ao executar procedure:", err)
+		if erroProcedure != "" {
+			utils.LogMonitor(utils.ErroFleury, label, "Erro ao processar instrucao: "+erroProcedure)
 			mensagemErro := ""
 			if err.Error() == "Dados não encontrados" {
 				mensagemErro = "Erro ao executar procedure: Produto não encontrado no MV"
@@ -71,6 +112,14 @@ func ProdutosFleury() gin.HandlerFunc {
 			})
 			return
 		}
+
+		//log.Println(mensagem)
+		//log.Println("IdProduto:", instrucaoFleury.IdProduto)
+		//log.Println("TextoInstrucao:", instrucaoFleury.TextoInstrucao)
+		// utils.LogMonitor(utils.Debug, label, "IdProduto: "+instrucaoFleury.IdProduto)
+		// utils.LogMonitor(utils.Debug, label, "TextoInstrucao: "+instrucaoFleury.TextoInstrucao)
+		// utils.LogMonitor(utils.Debug, label, "IdInstrucaoGeral: "+instrucaoFleury.IdInstrucaoGeral)
+
 		utils.LogMonitor(utils.Sucesso, label, "Produto processado com sucesso")
 		c.JSON(http.StatusOK, gin.H{
 			"sucesso": true,
