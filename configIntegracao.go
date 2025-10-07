e7dcf8a9360a8639116ee7a5746acbd7e5523404
R <dev>
2025-10-07 11:25:54 -0300

    versao sem get na fleury - retorno testado com sucesso

--- configIntegracao.go
+++ configIntegracao.go
@@ -102,7 +102,7 @@ func setupRouter() *gin.Engine {
 	r.GET("/check-status", AuthMiddleware(clientID, clientSecret), checkStatus())
 	r.GET("/produtos-fleury/:produto", AuthMiddleware(clientID, clientSecret), functions.ProdutosFleury())
 	r.GET("/produtos-fleury/todos", AuthMiddleware(clientID, clientSecret), functions.ProcessaProdutosFleury())
-
+	r.GET("/gera-pdf/:ficha", AuthMiddleware(clientID, clientSecret), functions.GeraPDF())
 	r.NoRoute(AuthMiddleware(clientID, clientSecret), NotFound())
 
 	return r
